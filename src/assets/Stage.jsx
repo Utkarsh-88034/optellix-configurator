df50126011822b2c7ed5fbf90a491b73672a507b
piyush-3d <assudani66@gmail.com>
2024-03-12 18:02:46 +0530

    🩹Front side issue

--- src/assets/Stage.jsx
+++ src/assets/Stage.jsx
@@ -4,20 +4,22 @@ Command: npx gltfjsx@6.2.16 .\Stage.glb -T
 Files: .\Stage.glb [2.33MB] > D:\optellix-configurator\public\Stage-transformed.glb [60.6KB] (97%)
 */
 
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { FrontSide } from "three";
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { FrontSide } from 'three';
 
 export function Stage_Props(props) {
   const { nodes, materials } = useGLTF("/Stage-transformed.glb");
   console.log(materials);
+  materials.White.side = FrontSide;
+  materials.Emission.side = FrontSide;
   console.log(window.innerWidth);
   return (
     <group {...props} dispose={null} scale={1}>
       <mesh geometry={nodes.stage_1.geometry} material={materials.Emission} />
       <mesh geometry={nodes.stage_2.geometry} material={materials.White} />
     </group>
-  );
+  )
 }
 
-useGLTF.preload("/Stage-transformed.glb");
+useGLTF.preload('/Stage-transformed.glb')
