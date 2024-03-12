20486f05b18e6e1c0ef0778a71b07c28071dfa21
Utkarsh-88034 <robbin7838@gmail.com>
2024-03-12 18:00:07 +0530

    .

--- src/assets/Stage.jsx
+++ src/assets/Stage.jsx
@@ -4,8 +4,9 @@ Command: npx gltfjsx@6.2.16 .\Stage.glb -T
 Files: .\Stage.glb [2.33MB] > D:\optellix-configurator\public\Stage-transformed.glb [60.6KB] (97%)
 */
 
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { FrontSide } from "three";
 
 export function Stage_Props(props) {
   const { nodes, materials } = useGLTF("/Stage-transformed.glb");
@@ -18,7 +19,7 @@ export function Stage_Props(props) {
       <mesh geometry={nodes.stage_1.geometry} material={materials.Emission} />
       <mesh geometry={nodes.stage_2.geometry} material={materials.White} />
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/Stage-transformed.glb')
+useGLTF.preload("/Stage-transformed.glb");
