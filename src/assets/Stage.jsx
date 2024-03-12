432edb1f98f24483d073ff13fc53537e9a56740e
Utkarsh-88034 <robbin7838@gmail.com>
2024-03-12 17:47:48 +0530

    .

--- src/assets/Stage.jsx
+++ src/assets/Stage.jsx
@@ -13,8 +13,9 @@ export function Stage_Props(props) {
   console.log(materials);
   materials.White.side = FrontSide;
   materials.Emission.side = FrontSide;
+  console.log(window.innerWidth);
   return (
-    <group {...props} dispose={null} scale={1}>
+    <group {...props} dispose={null} scale={window.innerWidth < 1000 ? 2 : 1}>
       <mesh geometry={nodes.stage_1.geometry} material={materials.Emission} />
       <mesh geometry={nodes.stage_2.geometry} material={materials.White} />
     </group>
